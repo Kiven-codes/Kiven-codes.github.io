a1117c0eb311190a47f4e9244d6b6b950c757ae9
Kiven-codes <jameskivenburce0@gmail.com>
2025-01-03 01:29:33 +1200

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,7 @@
+function displayText() {
+    var textField = document.getElementById('textField');
+    var loveGif = document.getElementById('loveGif');
+    textField.style.display = 'block';
+    textField.classList.add('show');
+    loveGif.style.display = 'block';
+}
